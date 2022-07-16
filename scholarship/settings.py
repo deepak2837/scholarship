2791d9bc70200eab1c1df82c74da8a22382b608c
Deepak <52418151+deepak2837@users.noreply.github.com>
2022-07-16 12:25:14 +0530

    Update settings.py

--- scholarship/settings.py
+++ scholarship/settings.py
@@ -82,13 +82,19 @@ WSGI_APPLICATION = 'scholarship.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 's2',
+#         'USER': 'postgres',
+#         'PASSWORD': 'NEW_PASSWORD',
+#         'HOST': 'localhost'
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 's2',
-        'USER': 'postgres',
-        'PASSWORD': 'NEW_PASSWORD',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
